4b9fad1410f3dfe1e2f4db58348b524f89001ef2
A620\1DAW <dandresalbarracin@gmail.com>
2024-10-16 20:51:27 +0200

    hola

--- iniciacion_java/src/main/java/org/example/Main.java
+++ iniciacion_java/src/main/java/org/example/Main.java
@@ -4,14 +4,7 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-<<<<<<< HEAD
-        CosasJava CosasJava = new CosasJava();
-        CosasJava.modos2();
-=======
         CosasJava cosasJava = new CosasJava();
-        CosasJava.modos();
->>>>>>> ae851b07a32ee0d1a0acc747e8a7f5c8d782cc75
+        cosasJava.modos2();
     }
 }
