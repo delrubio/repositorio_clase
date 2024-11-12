d4b9949932f3bd21fe7a94dee8afdc71eb01065b
A620\1DAW <dandresalbarracin@gmail.com>
2024-11-12 17:48:14 +0100

    samurais

--- tema3/src/main/java/org/example/samurais.java
+++ tema3/src/main/java/org/example/samurais.java
@@ -9,7 +9,7 @@ public class samurais {
         Random random = new Random();
         Scanner teclado = new Scanner(System.in);
 
-        int batalla = random.nextInt(8);
+        int batalla = random.nextInt(7);
         int equipo1[] = new int[7];
         int equipo2[] = new int[7];
         int suma = 0;
@@ -17,61 +17,76 @@ public class samurais {
         int puntos2 = 0;
 
         while (suma != 30) {
-            System.out.println("Equipo1: Introduce la potencia de los samurais: ");
-            String entrada = teclado.nextLine();
-            String[] equipo1_st = entrada.split(" ");
-            suma = 0;
+            try {
+                System.out.println("Equipo1: Introduce la potencia de los samurais: ");
+                String entrada = teclado.nextLine();
+                String[] equipo1_st = entrada.split(" ");
+                suma = 0;
 
-            for (int i = 0; i < 7; i++) {
-                equipo1[i] = Integer.parseInt(equipo1_st[i]);
-                suma = suma + equipo1[i];
+                for (int i = 0; i < 7; i++) {
+                    equipo1[i] = Integer.parseInt(equipo1_st[i]);
+                    suma = suma + equipo1[i];
+                }
+
+                if (suma != 30) {
+                    System.out.println("La potencia total tiene que ser 30");
+                    continue;
+                }
+                System.out.println("Equipo2: Introduce la potencia de los samurais: ");
+                String entrada2 = teclado.nextLine();
+                String[] equipo2_st = entrada2.split(" ");
+                suma = 0;
+
+                for (int i = 0; i < 7; i++) {
+                    equipo2[i] = Integer.parseInt(equipo2_st[i]);
+                    suma = suma + equipo2[i];
+                }
+
+                if (suma != 30) {
+                    System.out.println("La potencia total tiene que ser 30");
+                    continue;
+                }
             }
-
-            if (suma != 30){
-                System.out.println("La potencia total tiene que ser 30");
-                continue;
-            }
-            System.out.println("Equipo2: Introduce la potencia de los samurais: ");
-            String entrada2 = teclado.nextLine();
-            String[] equipo2_st = entrada.split(" ");
-            suma = 0;
-
-            for (int i = 0; i < 7; i++) {
-                equipo2[i] = Integer.parseInt(equipo2_st[i]);
-                suma = suma + equipo2[i];
-            }
-
-            if (suma != 30) {
-                System.out.println("La potencia total tiene que ser 30");
+            catch (NumberFormatException e1){
+                System.out.println("Error. Introduce un valor correcto." + e1);
                 continue;
             }
         }
-
-        for (int i = 0; i < 7; i++){
-            if (batalla == 7 && equipo1[batalla] > equipo2[batalla]){
+        for (int i = 0; i < equipo1.length; i++){
+            if (batalla == 6 && equipo1[batalla] > equipo2[batalla]){
                 System.out.println("Batalla " + i + " samurais " + batalla + ". Ganador el equipo1");
                 System.out.println("Resultado " + equipo1[batalla] + "vs" + equipo2[batalla]);
                 batalla = 0;
                 puntos1 ++;
-            }else if (batalla == 7 && equipo1[batalla] < equipo2[batalla]){
+                continue;
+            }else if (batalla == 6 && equipo1[batalla] < equipo2[batalla]){
                 System.out.println("Batalla " + i + " samurais " + batalla + ". Ganador el equipo2");
                 System.out.println("Resultado " + equipo1[batalla] + "vs" + equipo2[batalla]);
                 batalla = 0;
                 puntos2 ++;
-            }else if (batalla < 7 && equipo1[batalla] > equipo2[batalla]){
+                continue;
+            }else if (batalla == 6 && equipo1[batalla] == equipo2[batalla]){
+                System.out.println("Batalla " + i + " samurais " + batalla + ". Empate");
+                System.out.println("Resultado " + equipo1[batalla] + "vs" + equipo2[batalla]);
+                batalla = 0;
+                continue;
+            }else if (batalla < 6 && equipo1[batalla] > equipo2[batalla]){
                 System.out.println("Batalla " + i + " samurais " + batalla + ". Ganador el equipo1");
                 System.out.println("Resultado " + equipo1[batalla] + "vs" + equipo2[batalla]);
                 batalla ++;
                 puntos1 ++;
-            }else if (batalla < 7 && equipo1[batalla] < equipo2[batalla]){
+                continue;
+            }else if (batalla < 6 && equipo1[batalla] < equipo2[batalla]){
                 System.out.println("Batalla " + i + " samurais " + batalla + ". Ganador el equipo2");
                 System.out.println("Resultado " + equipo1[batalla] + "vs" + equipo2[batalla]);
                 batalla ++;
                 puntos2 ++;
+                continue;
             }else {
                 System.out.println("Batalla " + i + " samurais " + batalla + ". Empate");
                 System.out.println("Resultado " + equipo1[batalla] + "vs" + equipo2[batalla]);
                 batalla ++;
+                continue;
             }
         }
 
