85225b89c25084c65d9fd15a86eaac081ec42c8a
A620\1DAW <dandresalbarracin@gmail.com>
2024-11-19 18:15:16 +0100

    bv

--- tema3/src/main/java/org/example/primitiva.java
+++ tema3/src/main/java/org/example/primitiva.java
@@ -14,18 +14,18 @@ public class primitiva {
 
         boolean num = true;
 
-//        while (num) {
-//            try {
-//                System.out.println("LA PRIMITIVA: Loterías y Apuestas del Estado");
-//                System.out.println("Introduce los datos de tu boleto");
-//                String entrada = teclado.nextLine();
-//                num = entrada.matches("\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}/\\d");
-//                if (num) {
-//                    String[] numeros = entrada.split("[-/]");
-//                } else {
-//                    System.out.println("Algo no ha ido bien... Corrige el formato");
-//                    continue;
-//                }
+        while (num) {
+            try {
+                System.out.println("LA PRIMITIVA: Loterías y Apuestas del Estado");
+                System.out.println("Introduce los datos de tu boleto");
+                String apuesta = teclado.nextLine();
+                num = apuesta.matches("\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}/\\d");
+                if (num) {
+                    String[] numeros = apuesta.split("[-/]");
+                } else {
+                    System.out.println("Algo no ha ido bien... Corrige el formato");
+                    continue;
+                }
 //                for (int i = 0; i < numeros2.length; i++) {
 //                    numeros2[i] = Integer.parseInt(numeros[i]);
 //                    if (i == numeros2.length-1 ||numeros2[i] < 0 || numeros2[i] > 9){
@@ -40,24 +40,24 @@ public class primitiva {
 //                        num = false;
 //                    }
 //                }
-//            }catch (NumberFormatException e1){
-//                System.out.println("Error. Introduce un valor correcto." + e1);
-//                continue;
-//            }
-
-            for (int i=0; i< sorteo.length + 1; i++) {
-                int numero_random = random.nextInt(49)+1;
-                if (i == sorteo.length-2){
-                    int reintegro = random.nextInt(11);
-                }else if (i == sorteo.length -1){
-                    sorteo[i] = numero_random;
-                }else{
-                    int complementario = numero_random;
-                }
+            }catch (NumberFormatException e1){
+                System.out.println("Error. Introduce un valor correcto." + e1);
+                continue;
             }
-            Arrays.sort(sorteo);
+
+//            for (int i=0; i< sorteo.length + 1; i++) {
+//                int numero_random = random.nextInt(49)+1;
+//                if (i == sorteo.length-2){
+//                    int reintegro = random.nextInt(11);
+//                }else if (i == sorteo.length -1){
+//                    sorteo[i] = numero_random;
+//                }else{
+//                    int complementario = numero_random;
+//                }
+//            }
+//            Arrays.sort(sorteo);
 //            int limpio[] = Arrays.stream(numeros).distinct().toArray();
-            System.out.println(Arrays.toString(sorteo));
+//            System.out.println(Arrays.toString(sorteo));
         }
     }
-//}
+}
