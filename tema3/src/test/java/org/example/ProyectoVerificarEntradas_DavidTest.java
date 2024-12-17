9191029a4f4c00a2da85bf49a2d0300a9403ba43
DESKTOP-53G1GT2\dandr <dandresalbarracin@gmail.com>
2024-12-17 12:33:03 +0100

    asdad

---
+++ tema3/src/test/java/org/example/ProyectoVerificarEntradas_DavidTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProyectoVerificarEntradas_DavidTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void numeroEntradas() {
+    }
+
+    @Test
+    void verificacionEntradas() {
+    }
+
+    @Test
+    void convertirEntrada() {
+    }
+
+    @Test
+    void comprobarPares() {
+    }
+
+    @Test
+    void comprobarImpares() {
+    }
+}
