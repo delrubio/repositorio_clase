2063e761329075777ad2f351d39d185b107ddd54
DESKTOP-53G1GT2\dandr <dandresalbarracin@gmail.com>
2024-12-18 12:33:05 +0100

    Proyecto terminado

--- tema3/src/main/java/org/example/ProyectoVerificarEntradas_David.java
+++ tema3/src/main/java/org/example/ProyectoVerificarEntradas_David.java
@@ -2,14 +2,29 @@ package org.example;
 
 import java.util.Scanner;
 
+/**
+ * Proyecto de Olympiadas Navidad 2024. Verificación de Entradas Selección Española Femenina.
+ * @author David Andrés Albarracín
+ * @version 1.0 (18/12/2024)
+ */
+
 public class ProyectoVerificarEntradas_David {
 
     static Scanner teclado = new Scanner(System.in);
+
+    /**
+     *
+     * @param args
+     */
 
     public static void main(String[] args){
         int entradas = numeroEntradas();
         verificacionEntradas(entradas);
     }
+
+    /**
+     * @return Cogemos la entrada y lo devolvemos como un íntegro porque es un número
+     */
 
     public static int numeroEntradas(){
 
@@ -17,6 +32,14 @@ public class ProyectoVerificarEntradas_David {
 
         return entradas;
     }
+
+    /**
+     * @param entradas
+     * Hacemos un bucle que se repita según la cantidad de entradas que el usuario ha dicho que quiere verificar
+     * En un vector, metemos la entrada con un metodo que hemos llamado convertirEntrada()
+     * Enviamos ese vector a comprobarImpares() y a comprobarPares() que van a hacer cálculos con el vector
+     * El resultado de cada cálculo de comprobarImpares() y comprobarPares() lo sumamos y mostramos
+     */
 
     public static void verificacionEntradas(int entradas){
         for (int i = 0; i < entradas; i++) {
@@ -26,6 +49,13 @@ public class ProyectoVerificarEntradas_David {
             System.out.println(sumaImpares + sumaPares);
         }
     }
+
+    /**
+     * @param splitEntrada
+     * Metemos la entrada en un vector String haciendo un split para separar los números 1 a 1
+     * Tenemos que verificar que la longitud del vector sea un número par
+     * @return Si lo anterior sale correcto, convertimos el vector String a Íntegro para poder hacer cálculos
+     */
 
     public static int[] convertirEntrada(String splitEntrada){
         String[] numeros = splitEntrada.split("");
@@ -42,6 +72,14 @@ public class ProyectoVerificarEntradas_David {
 
         return codEntrada;
     }
+
+    /**
+     * @param pares
+     * Vamos a comprobar los números en las posiciones pares pero antes hay que mirar cual es el mayor
+     * número a la izquierda del que estamos comprobando actualmente
+     * Una vez mirado, cogemos el número, lo multiplicamos por 2 y los sumamos al numMayor
+     * @return Estas operaciones se guardan en una suma que se hace de los números en las posiciones pares
+     */
 
     public static int comprobarPares(int[] pares){
         int numMayor = 0;
@@ -62,6 +100,14 @@ public class ProyectoVerificarEntradas_David {
         return sumaPar;
     }
 
+    /**
+     * @param  impares
+     * Vamos a comprobar los números en las posiciones impares pero antes hay que mirar cual es el menor
+     * número a la derecha del que estamos comprobando actualmente
+     * Una vez mirado, cogemos el número, lo multiplicamos por 3 y los sumamos al numMenor
+     * @return Estas operaciones se guardan en una suma que se hace de los números en las posiciones impares
+     */
+
     public static int comprobarImpares(int[] impares){
         int numMenor = 0;
         int sumaImpar = 0;
@@ -79,7 +125,6 @@ public class ProyectoVerificarEntradas_David {
                 sumaImpar = (sumaImpar + (impares[i] * 3) + numMenor);
             }
         }
-
         return sumaImpar;
     }
 }
--- tema3/src/test/java/org/example/ProyectoVerificarEntradas_DavidTest.java
+++ tema3/src/test/java/org/example/ProyectoVerificarEntradas_DavidTest.java
@@ -1,10 +1,18 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * TESTS Proyecto de Verificación de Entradas Selección Española Femenina.
+ * @author David Andrés Albarracín
+ * @version 1.0 (18/12/2024)
+ */
+
 class ProyectoVerificarEntradas_DavidTest {
+
+    ProyectoVerificarEntradas_DavidTest validator = new ProyectoVerificarEntradas_DavidTest();
+
 
     @Test
     void main() {
@@ -16,10 +24,28 @@ class ProyectoVerificarEntradas_DavidTest {
 
     @Test
     void verificacionEntradas() {
+        assertTrue(validator.isNumeric("1234"));
+        assertFalse(validator.isNumeric("12a4"));
+        assertFalse(validator.isNumeric("-1234"));
+        assertFalse(validator.isNumeric("123@"));
+    }
+
+    private boolean isNumeric(String number) {
+        return false;
     }
 
     @Test
     void convertirEntrada() {
+        assertTrue(validator.hasEvenLength("12"));
+        assertTrue(validator.hasEvenLength("1762"));
+        assertFalse(validator.hasEvenLength("123"));
+        assertFalse(validator.hasEvenLength("abc"));
+        assertFalse(validator.hasEvenLength("-12"));
+        assertFalse(validator.hasEvenLength("12#4"));
+    }
+
+    private boolean hasEvenLength(String number) {
+        return false;
     }
 
     @Test
